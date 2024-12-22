4292a8c9ffbffc635ef35209ee5e03b008284667
Bilal Kamran <uroojkamran24@gmail.com>
2024-12-22 19:24:45 +0500

    Currently solving Advent of Code Day 01 part 02

--- AOC_day1/main.c
+++ AOC_day1/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "input1.h"
 #include "input2.h"
 #include "Bubblesort.h"
 
 int main() {
+    // PART 1
+    // Calculate the total distance between the two lists
+
     // Size of the two lists
     int n1 = INPUT1_S;
     int n2 = INPUT2_S;
@@ -24,6 +28,28 @@ int main() {
     }
 
     printf("The total distance b/w is: %d \n", total_distance);
+    // The answer for AOC day 01 part 01 is 2367773
+
+    // PART 2
+    // Calculate how many times a number has appeared in the list 2 and calculate its score
+    int total_score = 0;
+    int counter;
+
+    for (int i = 0; i < min_size; i++)
+    {
+        counter = 0;
+
+        for (int j = 0; j < min_size; j++)
+        {
+            if (input1[i] == input2[j])
+            {
+                counter ++;
+            }
+        }
+        total_score += input1[i] * counter;
+    }
+
+    printf("The total score of occurencies of all the elements of list one in list two is: %d\n", total_score);
 
     return 0;
 }
