9831d03f86fa66a19eb5fd3801c2be1783d83056
Bilal Kamran <uroojkamran24@gmail.com>
2024-12-24 14:26:50 +0500

    Solved Advent of Code Day 02 part 01

--- AOC_day2/input.h
+++ AOC_day2/input.h
@@ -4,6 +4,6 @@
 #define SIZEX 1000
 #define SIZEY 9
 
-extern int list[1000][9];
+extern int list[SIZEX][SIZEY];
 
 #endif // INPUT_H
--- AOC_day2/main.c
+++ AOC_day2/main.c
@@ -3,29 +3,77 @@
 #include <stdbool.h>
 #include "input.h"
 
-int arr_y_size (int arr[SIZEX][SIZEY]);
+int arr_y_size (int arr[SIZEX][SIZEY], int i);
+int sign (int x);
 
-int main()
-{
-}
+int main() {
 
-int arr_y_size (int arr[SIZEX][SIZEY]) {
+    // Advent of code day 01 part 01 SOLVED
+    // answer is 252
+
     int counter = 0;
+    int direction = 0;
+    int size_y;
 
     for (int i = 0; i < SIZEX; i++)
     {
-        for (int j = 0; j < SIZEY; j++)
+
+        bool is_monotonic = true;
+        size_y = arr_y_size(list, i);
+
+        for (int j = 0; j < size_y - 1; j++)
         {
-             if (arr[i][j] != -1)
+
+            int diff = list[i][j + 1] - list[i][j];
+
+            if (abs(diff) < 1 || abs(diff) > 3)
             {
-                counter++;
+                is_monotonic = false;
+                break;
             }
 
-            if (arr[i][j] == -1)
+            if (j == 0)
             {
+                direction = sign(diff);
+            }
+            else if (direction != sign(diff))
+            {
+                is_monotonic = false;
                 break;
             }
         }
+        printf("%d\n", is_monotonic);
+
+        if (is_monotonic)
+        {
+            counter++;
+        }
+    }
+
+    printf("%d\n", counter);
+
+    return 0;
+}
+
+int arr_y_size (int arr[SIZEX][SIZEY], int i) {
+    int counter = 0;
+
+    for (int j = 0; j < SIZEY; j++)
+    {
+        if (arr[i][j] != -1)
+        {
+            counter++;
+        }
+        else
+        {
+            break;
+        }
     }
     return counter;
+}
+
+int sign (int x) {
+    if (x > 0) return 1;
+    if (x < 0) return -1;
+    return 0;
 }
